ce1616a7ca5639e7aee6f2f00cf4af8b46b22d57
gpertaia <giorgipertaia@yahoo.com>
2013-10-03 12:30:43 -0700

    first dynamic app

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,11 +9,7 @@ describe PagesController do
       response.should be_success
     end
     
-    it "should have the right title"do
-      get 'home'
-      response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample App | Home")
-    end
+  
   end
 
     
@@ -25,11 +21,7 @@ describe PagesController do
       response.should be_success
     end
     
-    it "should have the right title"do
-      get 'contact'
-      response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample App | Contact")
-    end
+    
     
   end
     
@@ -42,11 +34,7 @@ describe PagesController do
       response.should be_success
     end
     
-    it "should have the right title"do
-      get 'about'
-      response.should have_selector("title",
-      :content =>  "Ruby on Rails Tutorial Sample App | About")
-    end
+   
     
   end
 
