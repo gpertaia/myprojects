af0cd6562a0e66d2a02a2900e07e250db0ceebb1
gpertaia <giorgipertaia@yahoo.com>
2013-10-05 12:07:09 -0700

    signi in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 TestApp2::Application.routes.draw do
   
 
+  get "sessions/new"
+
   get "users/new"
 
   get "pages/about"
@@ -12,11 +14,14 @@ TestApp2::Application.routes.draw do
 
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   root :to => 'pages#home'
 
